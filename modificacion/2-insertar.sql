10b1482e03077197e1b05794f0660af52fe92e98
joaquinsad <joaquinchehinismael@gmail.com>
2023-09-04 16:46:15 -0300

    insertar renombrado

---
+++ modificacion/2-insertar.sql
@@ -0,0 +1,8 @@
+--Inserts de usuario y post
+
+INSERT INTO post(author_id,title,body)
+VALUES(3,"PARTITION","spizeeee"),
+      (3,"PARTITION","spizeeee"),
+	  (3,"PARTITION","spizeeee"),
+	  (3,"PARTITION","spizeeee"),
+	  (3,"PARTITION","spizeeee")
